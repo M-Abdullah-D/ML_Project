a87b306869d5e8e55f7085b847fab436f302d6ff
M-Abdullah-D <m.abdullah.dukmak@gmail.com>
2024-04-14 16:28:58 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements(file:str)->list[str]:
+    requirements=[]
+    with open(file) as f:
+        requirements=f.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+    if "-e ." in requirements:
+        requirements.remove("-e .")
+    return requirements
+
+
+
+
+
+setup(
+    name='ML-Project-Template',
+    version='0.0.1',
+    author='Your Name',
+    author_email="",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt")
+    )
